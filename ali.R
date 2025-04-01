cf4b7b05e3de07e782179c51fd2fe1b0bffd8ae3
Andrew-dev1 <andrli20140@gmail.com>
2025-04-01 10:12:58 -0400

    completed most of the lab, just need to do writeup now

--- ali.R
+++ ali.R
@@ -24,19 +24,23 @@ calculate <- function(alpha, beta){
     )  
   return(dat.comparison)
 }
+
 betas <- tibble(alpha = c(2,5,5,0.5), beta = c(5,5,2,0.5))
 value.dat <- betas %>%
   pmap_dfr(~calculate(..1, ..2))
 
+# generate a grid of points and calculate beta values for different alphas and betas
 sequence <- seq(0, 1.0, length.out=500)
-figure1.data <- tibble(x = sequence)|>   # generate a grid of points
+figure1.data <- tibble(x = sequence)|>   
   mutate(beta.2.5 = dbeta(x, 2, 5),
          beta.5.5 = dbeta(x, 5, 5),
          beta.5.2 = dbeta(x, 5, 2),
          beta.half = dbeta(x, 0.5, 0.5)
          )
-beta.distributions <- ggplot(data= figure1.data)+                                            # specify data
-  geom_line(aes(x=x, y=beta.2.5, color="Beta(2,5)")) +                 # plot beta dist
+
+# plot beta distributions
+beta.distributions <- ggplot(data= figure1.data)+                     # specify data
+  geom_line(aes(x=x, y=beta.2.5, color="Beta(2,5)")) +                
   geom_line(aes(x=x, y=beta.5.5, color="Beta(5,5)")) + 
   geom_line(aes(x=x, y=beta.5.2, color="Beta(5,2)")) + 
   geom_line(aes(x=x, y=beta.half, color="Beta(0.5,0.5)")) + 
@@ -66,16 +70,19 @@ beta.moment <- function(alpha, beta, k, centered){
 
 beta.moment(2,3,4,T)
 
-# Task three
+
+# Task three: create random beta samples to view distributions
 set.seed(7272)
 
 sample.size <- 500
 beta.sample1 <- rbeta(n = sample.size,  # sample size
                      shape1 = 2,   # alpha parameter
                      shape2 = 5)    # beta parameter
+
 sample.df1 <- as.data.frame(beta.sample1)
 betaplot1 <- ggplot(sample.df1, aes(x= beta.sample1))+
-  geom_histogram(aes(y = after_stat(density)))
+  geom_histogram(aes(y = after_stat(density)),
+                 breaks = seq(from = 0, to = 1, by = 0.075))
 
 
 beta.sample2 <- rbeta(n = sample.size,  # sample size
@@ -83,7 +90,8 @@ beta.sample2 <- rbeta(n = sample.size,  # sample size
                       shape2 = 5)    # beta parameter
 sample.df2 <- as.data.frame(beta.sample2)
 betaplot2 <- ggplot(sample.df2, aes(x= beta.sample2))+
-  geom_histogram(aes(y = after_stat(density)))
+  geom_histogram(aes(y = after_stat(density)),
+                 breaks = seq(from = 0, to = 1, by = 0.075))
 
 
 beta.sample3 <- rbeta(n = sample.size,  # sample size
@@ -91,14 +99,16 @@ beta.sample3 <- rbeta(n = sample.size,  # sample size
                       shape2 = 2)    # beta parameter
 sample.df3 <- as.data.frame(beta.sample3)
 betaplot3 <- ggplot(sample.df3, aes(x= beta.sample3))+
-  geom_histogram(aes(y = after_stat(density)))
+  geom_histogram(aes(y = after_stat(density)),
+                 breaks = seq(from = 0, to = 1, by = 0.075))
 
 beta.sample4 <- rbeta(n = sample.size,  # sample size
                       shape1 = 0.5,   # alpha parameter
                       shape2 = 0.5)    # beta parameter
 sample.df4 <- as.data.frame(beta.sample4)
 betaplot4 <- ggplot(sample.df4, aes(x= beta.sample4))+
-  geom_histogram(aes(y = after_stat(density)))
+  geom_histogram(aes(y = after_stat(density)),
+                 breaks = seq(from = 0, to = 1, by = 0.075))
 
 (betaplot1 + betaplot2)
 (betaplot3 + betaplot4)
@@ -233,7 +243,10 @@ deaths.data <- read_csv(file = "Global deaths from World Bank/total_deaths.csv",
                    TRUE ~ y2022))
 
 ggplot(deaths.data)+
-  geom_histogram(aes(x =  y2022))+
+  geom_histogram(aes(x =  y2022, y = after_stat(density)),
+                 breaks = seq(from = 0.000, to = 0.024, by = 0.001),
+                 color = "black")+
+  stat_density(aes(x = y2022), geom="line")+
   theme_bw()
 
 MOM.beta <- function(data, par){
@@ -253,8 +266,8 @@ moms<- nleqslv(x = c(1,1),
                fn = MOM.beta,
                data=deaths.data$y2022)
 
-alpha.moms <- moms$par[1]
-beta.moms <- moms$par[2]
+alpha.moms <- moms$x[1]
+beta.moms <- moms$x[2]
 
 moms.internal <- MOM.beta(deaths.data$y2022, c(1,1))
 
@@ -274,24 +287,76 @@ mles <- optim(par = c(1, 1),
 alpha.mle <- mles$par[1]
 beta.mle <- mles$par[2]
 
+alpha = 8
+beta = 950
 result <- tibble()
 for(i in 1:1000){
   set.seed(7272+i)
   beta.sample <- rbeta(n = 266,      # sample size
-                       shape1 = 8,   # alpha parameter
-                       shape2 = 950) # beta parameter
+                       shape1 = alpha,   # alpha parameter
+                       shape2 = beta) # beta parameter
+
   moms.internal <- nleqslv(x = c(1,1),
                            fn = MOM.beta,
                            data= beta.sample)
-  mles.internal <- optim(par = c(1, 1),
+  mles.internal <- optim(par = c(1,1),
                          fn = MLE.beta,
                          data= beta.sample,
                          neg=T)
-  
-  
-  result <- bind_rows(result, tibble(moms.alpha = moms.internal$par[1], 
-                                     moms.beta = moms.internal$par[2],
-                                     mles.alpha = mles.internal$par[1], 
+  result <- bind_rows(result, tibble(moms.alpha = moms.internal$x[1],
+                                     moms.beta = moms.internal$x[2],
+                                     mles.alpha = mles.internal$par[1],
                                      mles.beta = mles.internal$par[2]))
 }
 
+plot.moms.alpha = ggplot(data = result, aes(x = moms.alpha)) + 
+  geom_density(fill = "#FD8A8A")+ 
+  theme_bw() +
+  geom_hline(yintercept=0)+
+  xlab("alpha values from moms")+
+  ggtitle("Estimated density for moms")
+
+plot.moms.beta = ggplot(data = result, aes(x = moms.beta)) + 
+  geom_density(fill = "#A8D1D1")+ 
+  theme_bw() +
+  geom_hline(yintercept=0)+
+  xlab("beta values from moms")+
+  ggtitle("Estimated density for moms")
+
+plot.mles.alpha = ggplot(data = result, aes(x = mles.alpha)) + 
+  geom_density(fill = "#d5d1e9")+ 
+  theme_bw() +
+  geom_hline(yintercept=0)+
+  xlab("alpha values from mles")+
+  ggtitle("Estimated density for mles")
+
+plot.mles.beta = ggplot(data = result, aes(x = mles.beta)) + 
+  geom_density(fill = "#f5cf9f")+ 
+  theme_bw() +
+  geom_hline(yintercept=0)+
+  xlab("beta values from mles")+
+  ggtitle("Estimated density for mles")
+
+(plot.moms.alpha + plot.moms.beta) / (plot.mles.alpha + plot.mles.beta)
+
+values = function(theta.hats, theta){
+  (bias <- mean(theta.hats) - theta)
+  (precision <- 1/var(theta.hats))
+  (mse <- var(theta.hats) + bias^2)
+  return(tibble(bias, precision, mse))
+}
+
+a.moms <- values(result$moms.alpha, alpha)
+b.moms <- values(result$moms.beta, beta)
+a.mles <- values(result$mles.alpha, alpha)
+b.mles <- values(result$mles.beta, beta)
+
+accuracy <- tibble()|> 
+  bind_rows(a.moms, b.moms, a.mles, b.mles) |> 
+  mutate(names = c("moms alpha", "moms beta", "mles alpha", "mles beta"),
+        actual = c(alpha, beta, alpha, beta),
+        estimated = c(mean(result$moms.alpha), mean(result$moms.beta), 
+                      mean(result$mles.alpha), mean(result$mles.beta))
+         )
+
+accuracy
